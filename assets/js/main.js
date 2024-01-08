49ca30de04854d71cd5c0ade0776d5b3b00b5f2c
Seona cha <139599643+seona-cha@users.noreply.github.com>
2024-01-08 11:01:22 +0900

    230108 수정
    (컨펌전 최종 수정)
    
    border-radius 수정
    motion 함수 가독성을 위한 수정 (setting 객체 추가)

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ const layoutChangeHandler = () => {
                     smallBox.classList.remove('image-box');
                     smallBox.classList.add('empty-box');
                 };
-            }
+            };
         };
 
         for(let i=0; i<lBoxNum; i++){
@@ -38,10 +38,9 @@ const layoutChangeHandler = () => {
             largeBox.classList.add(`l${i+1}`);
 
             Location.before(largeBox);
-        }
+        };
         document.querySelector(tLoca).after(titleBox);
-    }
-
+    };
 
     // mobile
     if(innerWidth <= 720){
@@ -69,12 +68,12 @@ const layoutChangeHandler = () => {
         const image = document.querySelectorAll('.image-box img');
         image.forEach((element)=>{
             let randomNumber = Math.floor(Math.random() * data.length);
-            element.setAttribute('src',`${data[randomNumber].imgUrl}`)
-            element.setAttribute('alt',`${data[randomNumber].alt}`)
-            data.splice(randomNumber,1)                             
-        })
+            element.setAttribute('src',`${data[randomNumber].imgUrl}`);
+            element.setAttribute('alt',`${data[randomNumber].alt}`);
+            data.splice(randomNumber,1);
+        });
 
-        // glitch 효과를 위한 레이어 생성
+        // glitch 효과 레이어
         const glitchEl = document.querySelectorAll('.image-box');
         glitchEl.forEach((element)=>{
             const layers = document.createElement('div');
@@ -85,16 +84,16 @@ const layoutChangeHandler = () => {
                 const layer = document.createElement('div');
                 layer.classList.add('glitch__layer');
                 layers.append(layer);
-            }
+            };
             
             const imgUrl = element.querySelector('img').getAttribute('src');
-            const EffectLayer = element.querySelectorAll('.glitch__layer')
+            const EffectLayer = element.querySelectorAll('.glitch__layer');
             EffectLayer.forEach((layer)=>{
-                layer.style.backgroundImage = `url(${imgUrl})`
-            })
-        })
+                layer.style.backgroundImage = `url(${imgUrl})`;
+            });
+        });
 
-        // popup layer
+        // 레이어 팝업
         const popupWrapper = document.querySelector('.popup-wrapper');
         const popupInner = document.querySelector('.popup-inner');
         const imageBox = document.querySelectorAll('.image-box img');
@@ -102,35 +101,38 @@ const layoutChangeHandler = () => {
         const img = document.createElement('img');
         imageBox.forEach(element => {
             element.addEventListener("click",function(){
-                img.setAttribute('src',element.getAttribute('src'))
+                img.setAttribute('src',element.getAttribute('src'));
 
                 popupWrapper.classList.add('on');
                 popupInner.append(img);
-                setTimeout(function(){
-                    popupWrapper.addEventListener("click",dimmedClicked)
-                },500)
-            })
-        })
-
-            // 닫기
-            function popupClose(){
-                popupWrapper.style.animationName = "fadeOut";
-                popupInner.style.animationName = "flipOut";
 
                 setTimeout(function(){
-                    popupWrapper.classList.remove('on');
-                    img.remove();
-                    popupWrapper.style.animationName = "fadeIn";
-                    popupInner.style.animationName = "flipIn";
-                },300)
-            }
-            function dimmedClicked(e){
-                if(e.target != document.querySelector('.popup-inner img')){
-                    popupClose();
-                    popupWrapper.removeEventListener("click",dimmedClicked)
-                }
-            }
-            document.querySelector('.close-btn').addEventListener("click",popupClose)
+                    popupWrapper.addEventListener("click",dimmedClicked);
+                },500);
+            });
+        });
+
+        // 닫기 - 버튼 클릭시
+        function popupClose(){
+            popupWrapper.style.animationName = "fadeOut";
+            popupInner.style.animationName = "flipOut";
+
+            setTimeout(function(){
+                popupWrapper.classList.remove('on');
+                img.remove();
+                popupWrapper.style.animationName = "fadeIn";
+                popupInner.style.animationName = "flipIn";
+            },300);
+        };
+
+        // 닫기 - 이미지 바깥 영역 클릭시
+        function dimmedClicked(e){
+            if(e.target != document.querySelector('.popup-inner img')){
+                popupClose();
+                popupWrapper.removeEventListener("click",dimmedClicked);
+            };
+        };
+        document.querySelector('.close-btn').addEventListener("click",popupClose);
 
     });
 
@@ -139,11 +141,10 @@ const layoutChangeHandler = () => {
 // -- Layout Setting --//
 
 const mediaQueryList = window.matchMedia(`(max-width: 720px)`);
+
+window.addEventListener('load',layoutChangeHandler);
 mediaQueryList.addEventListener('change', layoutChangeHandler);
 
-window.addEventListener('load',function(){
-    layoutChangeHandler();
-});
 
 // ---- Motions ---- //
 function getOffset(element, vw, max, direction){
@@ -160,41 +161,39 @@ function getOffset(element, vw, max, direction){
     return offset;
 }
 
-function motionX(element, opacity, duration, delay, direction){
+function motionX(element, setting){
     const box = document.querySelector(element);
-
-    let offset = 0;
-    offset = getOffset(box, 0.007, 12, direction);
+    const offset = getOffset(box, 0.007, 12, setting.direction);
     
     box.style.transform = `translateX(${offset})`;
-    box.style.opacity = opacity;
+    box.style.opacity = setting.opacity;
 
     box.animate([
-        {transform:`translateX(${offset})`, opacity:opacity, easing:"ease"},
+        {transform:`translateX(${offset})`, opacity:setting.pacity, easing:"ease"},
         {transform:'translateX(0)', opacity:1}
     ],{
-        duration:duration, 
-        delay:delay,
-        fill:"forwards"
+        duration: setting.duration, 
+        delay: setting.delay,
+        fill: "forwards"
     });
 }
 
-function motionY(element, opacity, duration, delay, direction){
+function motionY(element, setting){
     const box = document.querySelector(element);
 
     let offset = 0;
-    offset = getOffset(box, 0.0073, 13.5, direction);
+    offset = getOffset(box, 0.0073, 13.5, setting.direction);
 
     box.style.transform = `translateY(${offset})`;
-    box.style.opacity = opacity;
+    box.style.opacity = setting.opacity;
 
     box.animate([
-        {transform:`translateY(${offset})`, opacity:opacity, easing:"ease"},
+        {transform:`translateY(${offset})`, opacity:setting.opacity, easing:"ease"},
         {transform:'translateY(0)', opacity:1}
     ],{
-        duration:duration, 
-        delay:delay,
-        fill:"forwards"
+        duration: setting.duration, 
+        delay: setting.delay,
+        fill: "forwards"
     });
 }
 // ---- Motions ---- //
@@ -203,36 +202,36 @@ function motionY(element, opacity, duration, delay, direction){
 // -- PC Motion Setting -- //
 function layoutMotionPC() {
     // (element, opacity, duration, delay, direction)
-    motionY('.l2',1,500,500,-1);
-    motionX('.s1',0,600,700,-1);
-    motionX('.l3',1,300,300,1);
-    motionY('.s3',0,500,700,1);
-    motionX('.s4',1,400,400,-1);
-    motionY('.s5',0,600,300,-1);
-    motionY('.s6',1,400,1400,-1);
-    motionY('.s7',1,600,900,1);
-    motionY('.s10',1,400,200,-1);
-    motionY('.s11',1,400,400,1);
-    motionX('.s12',1,400,1000,1);
-    motionX('.s13',0,600,1700,1);
-    motionY('.s14',0,500,1200,1);
-    motionX('.s18',1,400,800,-1);
-    motionY('.s17',0,400,1100,1);
-    motionX('.s20',0,500,1200,1);
+    motionY('.l2',{opacity:1, duration:500, delay:500, direction:-1});
+    motionX('.s1',{opacity:0, duration:600, delay:700, direction:-1});
+    motionX('.l3',{opacity:1, duration:300, delay:300, direction:1});
+    motionY('.s3',{opacity:0, duration:500, delay:700, direction:1});
+    motionX('.s4',{opacity:1, duration:400, delay:400, direction:-1});
+    motionY('.s5',{opacity:0, duration:600, delay:300, direction:-1});
+    motionY('.s6',{opacity:1, duration:400, delay:1400, direction:-1});
+    motionY('.s7',{opacity:1, duration:600, delay:900, direction:1});
+    motionY('.s10',{opacity:1, duration:400, delay:200, direction:-1});
+    motionY('.s11',{opacity:1, duration:400, delay:400, direction:1});
+    motionX('.s12',{opacity:1, duration:400, delay:1000, direction:1});
+    motionX('.s13',{opacity:0, duration:600, delay:1700, direction:1});
+    motionY('.s14',{opacity:0, duration:500, delay:1200, direction:1});
+    motionX('.s18',{opacity:1, duration:400, delay:800, direction:-1});
+    motionY('.s17',{opacity:0, duration:400, delay:1100, direction:1});
+    motionX('.s20',{opacity:0, duration:500, delay:1200, direction:1});
 }
 // -- PC Motion Setting -- //
 
 // -- Mobile Motion Setting -- //
 function layoutMotionMobile() {
     // (element, opacity, duration, delay, direction)
-    motionY('.l1',1,400,1000,-1);
-    motionY('.s1',0,600,1600,-1);
-    motionX('.s2',1,400,800,-1);
-    motionX('.s3',1,600,500,-1);
-    motionX('.s4',0,500,1400,1);
-    motionX('.s6',1,400,600,1);
-    motionX('.s7',0,600,1200,-1);
-    motionX('.s11',1,400,800,1);
-    motionY('.s12',0,600,1200,1);
+    motionY('.l1',{opacity:1, duration:400, delay:1000, direction:-1});
+    motionY('.s1',{opacity:0, duration:600, delay:1600, direction:-1});
+    motionX('.s2',{opacity:1, duration:400, delay:800, direction:-1});
+    motionX('.s3',{opacity:1, duration:600, delay:500, direction:-1});
+    motionX('.s4',{opacity:0, duration:500, delay:1400, direction:1});
+    motionX('.s6',{opacity:1, duration:400, delay:600, direction:1});
+    motionX('.s7',{opacity:0, duration:600, delay:1200, direction:-1});
+    motionX('.s11',{opacity:1, duration:400, delay:800, direction:1});
+    motionY('.s12',{opacity:0, duration:600, delay:1200, direction:1});
 }
 // -- Mobile Motion Setting -- //
